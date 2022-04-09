320e5af7d4c9f3765458c7ebeb10082e724e4f24
Altino Alves Júnior <altinoalvesjunior@gmail.com>
2022-04-09 18:17:05 -0300

    adicionando função para filtro

--- request.py
+++ request.py
@@ -2,26 +2,52 @@ import requests
 
 def getRepositories():
     url = 'https://api.github.com/graphql'
-    token = "TOKEN AQUI"
+    token = "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"
     headers = {"Authorization": "Bearer " + token}
 
     query = """
-    query getRepoInfo {
-        search(query: "stars:>100", type: REPOSITORY, first: 25) {
-            nodes {
-              ... on Repository {
-                nameWithOwner
-                url
-                createdAt
-                primaryLanguage {
-                  name
-                  id
-                }
-              }
+    {
+        search(query:"stars:>100", type:REPOSITORY, first:5){
+            pageInfo {
+                startCursor
+                endCursor
+                hasNextPage
             }
+            nodes {
+            ... on Repository {
+                    name
+                    url
+                    createdAt
+                    updatedAt
+              
+                    pullRequestMerged: pullRequests(states: MERGED){
+                        totalCount
+                    }
+              
+                    pullRequestClosed: pullRequests(states: CLOSED){
+                        totalCount
+                    }
+              
+                    owner {
+                        login
+                    }
+                } 
+            } 
         }
     }
     """
 
     request = requests.post(url, json={'query': query}, headers=headers)
-    print(request.content)
+    filterRepository(request)
+
+def filterRepository(request):
+    # print(request.z)
+    jsonResponse = request.json()
+    repository = jsonResponse['data']['search']['nodes'][0]
+    print(f'Repository name: {repository["name"]}')
+
+    pullRequestMergedCount = repository["pullRequestMerged"]['totalCount']
+    pullRequestClosedCount = repository["pullRequestClosed"]['totalCount']
+    print(pullRequestMergedCount)
+    print(pullRequestClosedCount)
+
